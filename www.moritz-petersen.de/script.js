739b4cb556895fe3c0bf0164f121f064729652e3
Aman Mehta <amanmehta122003@gmail.com>
2022-08-22 19:25:25 +0530

    add

--- www.moritz-petersen.de/script.js
+++ www.moritz-petersen.de/script.js
@@ -24562,19 +24562,19 @@
                   e ||
                   ((r = t('<a class="w-webflow-badge"></a>').attr(
                     "href",
-                    "https://webflow.com?utm_campaign=brandjs"
+                    ""
                   )),
                   (n = t("<img>")
                     .attr(
                       "src",
-                      "https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-icon.f67cd735e3.svg"
+                      ""
                     )
                     .attr("alt", "")
                     .css({ marginRight: "8px", width: "16px" })),
                   (s = t("<img>")
                     .attr(
                       "src",
-                      "https://d1otoma47x30pg.cloudfront.net/img/webflow-badge-text.6faa6a38cd.svg"
+                      ""
                     )
                     .attr("alt", "Made in Webflow")),
                   r.append(n, s),
